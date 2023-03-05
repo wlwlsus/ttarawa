e0850b4bdc8a767a11d39d2e64339454ee919563
swj <cadqe13@gmail.com>
2023-03-05 17:08:07 +0900

    first commit

---
+++ src/main/java/com/ssafy/ttarawa/TtarawaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ttarawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtarawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtarawaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/ttarawa/TtarawaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ttarawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtarawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
