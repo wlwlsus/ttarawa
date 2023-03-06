7da81f493a380afd1753ca025b2d855a43309a79
두소원 <sowondoo@naver.com>
2023-03-06 17:34:40 +0900

    BE: Feat: create security config

--- ttarawa/src/main/java/com/jsdckj/ttarawa/config/security/WebSecurityConfig.java
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/config/security/WebSecurityConfig.java
@@ -1,4 +1,24 @@
 package com.jsdckj.ttarawa.config.security;
 
-public class WebSecurityConfig {
+import com.jsdckj.ttarawa.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig   {
+
+  private final JwtTokenProvider jwtTokenProvider;
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http.build();
+  }
+
+
 }
