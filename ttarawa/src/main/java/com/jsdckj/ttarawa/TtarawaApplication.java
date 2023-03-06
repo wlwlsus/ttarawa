5ab911c67ea99328b6c535da20db9c094f9118d5
성원준 <cadqe13@gmail.com>
2023-03-06 10:22:06 +0900

    BE:Add: Backend Project

---
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/TtarawaApplication.java
@@ -0,0 +1,13 @@
+package com.jsdckj.ttarawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtarawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtarawaApplication.class, args);
+	}
+
+}
---
+++ ttarawa/src/test/java/com/jsdckj/ttarawa/TtarawaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsdckj.ttarawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtarawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
