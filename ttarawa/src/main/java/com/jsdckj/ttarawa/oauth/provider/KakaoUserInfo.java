fac1b4708e805d9015d7351f697fa991bfa2b947
두소원 <sowondoo@naver.com>
2023-03-09 17:29:22 +0900

    BE: Feat: create Kakao User Information

---
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/oauth/provider/KakaoUserInfo.java
@@ -0,0 +1,37 @@
+package com.jsdckj.ttarawa.oauth.provider;
+
+import java.util.Map;
+
+public class KakaoUserInfo implements OAuth2UserInfo {
+
+  private final Map<String, Object> attributes;
+  private final Map<?, ?> properties;
+  private final Map<?, ?> account;
+
+
+  public KakaoUserInfo(Map<String, Object> attributes) {
+    this.attributes = attributes;
+    properties = (Map<?, ?>) attributes.get("properties");
+    account = (Map<?, ?>) attributes.get("kakao_account");
+  }
+
+  @Override
+  public String getProvider() {
+    return "kakao";
+  }
+
+  @Override
+  public String getEmail() {
+    return account.get("email").toString();
+  }
+
+  @Override
+  public String getNickname() {
+    return properties.get("nickname").toString();
+  }
+
+  @Override
+  public String getProfileImg() {
+    return properties.get("profile_image").toString();
+  }
+}
