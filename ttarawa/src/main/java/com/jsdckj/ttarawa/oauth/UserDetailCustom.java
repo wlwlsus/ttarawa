aa93a0f81f557907562913addd1da9e404408bf9
두소원 <sowondoo@naver.com>
2023-03-09 17:29:44 +0900

    BE: Feat: create User Detail Custom

---
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/oauth/UserDetailCustom.java
@@ -0,0 +1,43 @@
+package com.jsdckj.ttarawa.oauth;
+
+import com.jsdckj.ttarawa.users.entity.Users;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@Data
+public class UserDetailCustom implements OAuth2User {
+
+  private final Users users;
+  private Map<String, Object> attributes;
+
+  public UserDetailCustom(Users users) {
+    this.users = users;
+  }
+
+  public UserDetailCustom(Users users, Map<String, Object> attributes) {
+    this.users = users;
+    this.attributes = attributes;
+  }
+
+  @Override
+  public Map<String, Object> getAttributes() {
+    return attributes;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    Collection<GrantedAuthority> collection = new ArrayList<>();
+    collection.add(users::getEmail);
+    return collection;
+  }
+
+  @Override
+  public String getName() {
+    return users.getNickname();
+  }
+}
