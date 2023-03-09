f5b07a26ec428e5b67987a99fae3e9a7f5652fb2
두소원 <sowondoo@naver.com>
2023-03-09 15:56:59 +0900

    BE: Feat: add method for login

--- ttarawa/src/main/java/com/jsdckj/ttarawa/users/repository/UserRepository.java
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/users/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.jsdckj.ttarawa.users.repository;
 import com.jsdckj.ttarawa.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<Users, Long> {
+
+  Optional<Users> findByEmailAndProvider(String email, String provider);
+
 }
