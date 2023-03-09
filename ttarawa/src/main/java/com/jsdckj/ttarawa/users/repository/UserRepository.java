adf76bc6d97da11856d2f4101c6de9e5500bdbef
두소원 <sowondoo@naver.com>
2023-03-09 12:25:23 +0900

    BE: Feat: config JPA repository for UserRepository

--- ttarawa/src/main/java/com/jsdckj/ttarawa/users/repository/UserRepository.java
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/users/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.jsdckj.ttarawa.users.repository;
 
-public interface UserRepository {
+import com.jsdckj.ttarawa.users.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
 }
