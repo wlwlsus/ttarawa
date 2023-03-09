f15816c27067e1ce93c64d0f00b1c308fc501d46
두소원 <sowondoo@naver.com>
2023-03-09 12:24:32 +0900

    BE: Feat: add some properties for JWT Token

--- ttarawa/src/main/java/com/jsdckj/ttarawa/jwt/JwtProperties.java
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/jwt/JwtProperties.java
@@ -4,6 +4,7 @@ public interface JwtProperties {
 
   String TOKEN_HEADER = "Authorization";
   String BEARER_TYPE = "Bearer ";
+  String AUTHORITIES_KEY = "auth";
   long ACCESS_TOKEN_EXPIRE_TIME = 30 * 60 * 1000L; //30분
   long REFRESH_TOKEN_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000L;// 7일
 
