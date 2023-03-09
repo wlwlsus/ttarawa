7b2bf7237ee8343fbc30260b4b96c66943162559
두소원 <sowondoo@naver.com>
2023-03-09 15:57:48 +0900

    BE: Fix: extends basetime

--- ttarawa/src/main/java/com/jsdckj/ttarawa/users/entity/Users.java
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/users/entity/Users.java
@@ -1,5 +1,6 @@
 package com.jsdckj.ttarawa.users.entity;
 
+import com.jsdckj.ttarawa.util.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -9,7 +10,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Table(name="users")
-public class Users {
+public class Users extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
