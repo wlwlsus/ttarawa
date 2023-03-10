2d3da9faa4a43f5b10c293617bf75c18bfd0288f
CSB <tph5268@naver.com>
2023-03-10 15:29:01 +0900

    FE:Feat: IconButton component

--- ttarawa/src/styles/GlobalStyles.ts
+++ ttarawa/src/styles/GlobalStyles.ts
@@ -5,9 +5,11 @@ export const styles = StyleSheet.create({})
 export const color = {
   primary: '#2155CD',
   secondary: '#C9E2FF',
+  white: '#FFFFFF',
   gray: '#B5B5B5',
   lightGray: '#D9D9D9',
   yellow: '#FEE500',
   red: '#F22929',
   green: '#00C73C',
+  black: '#000000',
 }
