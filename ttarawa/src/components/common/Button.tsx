d43099f9d2575fdf1de3f530bb7804ec6a34dd8e
Yeji <yzii9841@gmail.com>
2023-03-10 14:58:48 +0900

    FE:Fix: Button component

--- ttarawa/src/components/common/Button.tsx
+++ ttarawa/src/components/common/Button.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Pressable } from 'react-native'
+import { StyleSheet, Text, Pressable } from 'react-native'
 import { color } from '@styles/GlobalStyles'
 
 export default function Button(props: {
@@ -6,60 +6,68 @@ export default function Button(props: {
   style: string
   press: (params: any) => any // type check off (수정 필요)
 }) {
-  const btnStyle: [object, object] =
+  const btnStyle: { btn: object; text: object } =
     props.style === 'blue'
-      ? [styles.blueBtn, styles.blueBtnText]
+      ? { btn: styles.blueBtn, text: styles.blueBtnText }
       : props.style === 'white'
-      ? [styles.whiteBtn, styles.whiteBtnText]
-      : [styles.tabBtn, styles.tabBtnText]
+      ? { btn: styles.whiteBtn, text: styles.whiteBtnText }
+      : { btn: styles.tabBtn, text: styles.tabBtnText }
 
   return (
-    <Pressable hitSlop={50} onPress={props.press}>
-      <View style={btnStyle[0]}>
-        <Text style={btnStyle[1]}>{props.text}</Text>
-      </View>
+    <Pressable
+      hitSlop={50}
+      style={[styles.container, btnStyle.btn]}
+      onPress={props.press}
+    >
+      <Text style={[styles.text, btnStyle.text]}>{props.text}</Text>
     </Pressable>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    borderRadius: 10,
+
+    shadowColor: color.shadow,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.0,
+
+    elevation: 1,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 25,
+    fontWeight: '500',
+  },
+
   blueBtn: {
     backgroundColor: color.primary,
     padding: 10,
     marginHorizontal: 20,
-    borderRadius: 10,
   },
-
   blueBtnText: {
-    textAlign: 'center',
     color: color.white,
-    fontSize: 25,
-    fontWeight: '600',
   },
 
   whiteBtn: {
     backgroundColor: color.white,
     paddingVertical: 20,
     marginHorizontal: 20,
-    borderRadius: 10,
   },
-
   whiteBtnText: {
-    textAlign: 'center',
     color: color.primary,
     fontSize: 25,
-    fontWeight: '600',
   },
 
   tabBtn: {
     backgroundColor: color.white,
-    borderRadius: 10,
     padding: 5,
   },
-
   tabBtnText: {
-    textAlign: 'center',
     fontSize: 20,
-    fontWeight: '500',
   },
 })
