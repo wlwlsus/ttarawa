83cc4bd98da35c4dd72e6093b9de989f8f96ed82
Yeji <yzii9841@gmail.com>
2023-03-10 17:59:34 +0900

    FE:Feat: BottomTab

--- ttarawa/App.tsx
+++ ttarawa/App.tsx
@@ -1,30 +1,78 @@
-import { View, Text } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native' // 전체 네비게이션을 감싸는 컨테이너 불러오기
-import { createStackNavigator } from '@react-navigation/stack' // 스택 네비게이션 라이브러리 불러오기
-// import { store } from '@store/index'
-// import { Provider } from 'react-redux'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import Index from '@screens/index'
 import Main from '@screens/main'
 import Mypage from '@screens/mypage'
 import Sns from '@screens/sns'
 
+import { color } from '@styles/GlobalStyles'
+
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { Zocial } from '@expo/vector-icons'
+
 const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    // <Provider store={store}>
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Index" // 초기(메인)페이지 설정
-        screenOptions={{ headerShown: false }} // 헤더 숨기기
+        initialRouteName="MainTabs"
+        screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Index" component={Index} />
-        <Stack.Screen name="Main" component={Main} />
-        <Stack.Screen name="Mypage" component={Mypage} />
-        <Stack.Screen name="Sns" component={Sns} />
+        <Stack.Screen name="MainTabs" component={MainTabs} />
       </Stack.Navigator>
     </NavigationContainer>
-    // </Provider>r
+  )
+}
+
+function MainTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{ headerShown: false, tabBarShowLabel: false }}
+    >
+      <Tab.Screen
+        name="Sns"
+        component={Sns}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name="format-list-checkbox"
+              size={50}
+              color={focused ? color.primary : color.lightGray}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Main"
+        component={Main}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name="map-marker-radius"
+              size={50}
+              color={focused ? color.primary : color.lightGray}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Mypage"
+        component={Mypage}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Zocial
+              name="persona"
+              size={40}
+              color={focused ? color.primary : color.lightGray}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   )
 }
--- ttarawa/src/screens/index.tsx
+++ ttarawa/src/screens/index.tsx
@@ -1,5 +1,14 @@
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 
 export default function Index() {
-  return <View></View>
+  return (
+    <View>
+      <Text>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero,
+        corrupti provident cumque, consequatur esse illum quaerat quasi labore
+        ipsam id reiciendis facere ab unde assumenda porro, expedita aperiam
+        ipsa officiis?
+      </Text>
+    </View>
+  )
 }
