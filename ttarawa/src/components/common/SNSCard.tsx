3149728a3a901d53ab53938ec8a7ff0ffb39d9b7
hyejin0705 <hyejin3681@gmail.com>
2023-03-14 13:47:31 +0900

    FE:Feat: SNSCard component CSS

--- ttarawa/src/components/common/SNSCard.tsx
+++ ttarawa/src/components/common/SNSCard.tsx
@@ -3,78 +3,83 @@ import { StyleSheet, View, Text, Image } from 'react-native'
 import { color } from '@styles/GlobalStyles'
 // import getImageSize from '@utils/getSize'
 import IconButton from './IconButton'
-import { MaterialIcons } from '@expo/vector-icons'
+// import { MaterialIcons } from '@expo/vector-icons'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { Ionicons } from '@expo/vector-icons'
 import Label from './Label'
 
-
 export default function Card(props: {
-  imagepath?: (params: any) => any   // require()  >>  image url 함수
-  likes?: string                     // 좋아요 수
-  content?: string                   // 내용
-  isLike?: boolean                   // 좋아요 누른 여부
-  distence?: string                  // 주행거리  ex) 3.5 km
-  hour?: string                      // 주행시간  ex) 30분
+  imagepath?: (params: any) => any // require()  >>  image url 함수
+  likes?: string // 좋아요 수
+  content?: string // 내용
+  isLike?: boolean // 좋아요 누른 여부
+  distence?: string // 주행거리  ex) 3.5 km
+  hour?: string // 주행시간  ex) 30분
   userName?: string
-  profileImage?: (params: any) => any
+  userImg?: (params: any) => any
   rank?: (params: any) => any
 }) {
-
   // 내용 띄어쓰기를 위한
   const contentText = props.content?.replace(/(!!|\?|\.)/g, '$&\n')
 
   const distanceText = `주행 거리 : ${props.distence}`
   const hourText = `주행 시간 : ${props.hour}`
-  
+
   // 좋아요 여부 저장 변수 (props로 넘길???)
   const [like, setLike] = useState(props.isLike === 'true' ? true : false)
 
   return (
-    <View>
+    <View style={styles.cardContainer}>
       {/* ID, Profile */}
-      <View style={styles.rowSort}>
-        {/* <Image source={props.profileImage} style={{width: 30}} /> */}
-        <Text>{props.userName}</Text>
+      <View style={styles.userInfo}>
+        <View style={styles.imgContainer}>
+          <Image source={props.userImg} style={styles.userImg} />
+        </View>
+        <Text style={styles.userName}>{props.userName}</Text>
         <Image source={props.rank} />
       </View>
-      
-      <View style={styles.container}>
-        {/* 경로 이미지 */}
-        <Image source={props.imagepath} style={styles.imageCard} />
 
+      {/* 경로 이미지 */}
+      <Image source={props.imagepath} style={styles.cardImg} />
+
+      <View style={styles.cardContent}>
         {/*  like */}
-        <View style={styles.rowSort}>
-          <View style={styles.rowSort}>
+        <View style={styles.likeContainer}>
+          <View style={styles.like}>
             <IconButton
               icon1={
-                <MaterialIcons
-                  name="local-fire-department"
-                  size={24}
-                  color= {like ? "crimson" : "black"}   // 좋아요 누르면, 색 변환
+                <Ionicons
+                  name="heart-sharp"
+                  size={35}
+                  color={like ? 'crimson' : 'black'} // 좋아요 누르면, 색 변환
                 />
               }
-              press={() => {setLike(!like)}}    // 누르면, true <-> false 함수
+              press={() => {
+                setLike(!like)
+              }} // 누르면, true <-> false 함수
             />
-            <Text style={styles.likeText}>{props.likes}명</Text>
+            <Text style={styles.likeNum}>{props.likes}명</Text>
             <Text style={styles.contentText}>
               이 이 코스를 달리고 싶어합니다.
             </Text>
           </View>
-          
-          {/* 공유 && 수정 아이콘 */}
-          <View style={{ paddingHorizontal: 10 }}>
-            {props.userID ? (
-              // SNS 공유 아이콘
-              <IconButton 
+
+          {/* userName 유무에 따라 공유 or 수정 아이콘 */}
+
+          <View>
+            {props.userName ? (
+              <IconButton
                 icon1={
-                  <Ionicons name="paper-plane-outline" size={24} color="black" />
+                  <Ionicons
+                    name="paper-plane-outline"
+                    size={24}
+                    color="black"
+                  />
                 }
                 // press
               />
             ) : (
-              // 수정 아이콘
-              <IconButton 
+              <IconButton
                 icon1={
                   <MaterialCommunityIcons
                     name="dots-vertical"
@@ -89,51 +94,107 @@ export default function Card(props: {
         </View>
 
         {/* Label */}
-        <View style={styles.labelSort}>
+        <View style={styles.label}>
           <Label text={distanceText} />
           <Label text={hourText} />
         </View>
-        
+
         {/* 내용 */}
-        <Text style={styles.contentText}>{contentText}</Text>
+        <Text ellipsizeMode="tail" numberOfLines={5} style={styles.cardText}>
+          {contentText}
+        </Text>
       </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: color.white,
-    borderRadius: 20,
+  cardContainer: {
+    flex: 1,
+    marginHorizontal: 10,
   },
 
-  rowSort: {
+  userInfo: {
     flexDirection: 'row',
-    alignContent: 'center',
+    alignItems: 'center',
+    gap: 7,
+    marginHorizontal: 10,
+    marginVertical: 5,
+  },
+
+  imgContainer: {
+    borderRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: color.secondary,
+  },
+
+  userImg: {
+    margin: 5,
+    width: 40,
+    height: 40,
+  },
+
+  userName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+  cardImg: {
+    width: '100%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+
+  cardContent: {
+    backgroundColor: color.white,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    paddingHorizontal: 20,
+    flex: 0.55,
+
+    shadowColor: color.shadow,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+
+    elevation: 3,
+  },
+
+  likeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'space-between',
   },
 
-  imageCard: {
-    borderRadius: 10,
-    width: '100%',
-    // aspectRatio: 'auto',
+  like: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 
-  likeText: {
-    fontSize: 25,
+  likeNum: {
+    fontSize: 18,
     fontWeight: 'bold',
     paddingVertical: 10,
   },
 
   contentText: {
-    fontSize: 20,
-    paddingVertical: 15,
+    fontSize: 15,
   },
 
-  labelSort: {
+  label: {
     flexDirection: 'row',
     alignContent: 'center',
-    paddingHorizontal: 10,
     gap: 10,
+  },
+
+  cardText: {
+    marginVertical: 10,
+    fontSize: 15,
+    flex: 1,
   },
 })
--- ttarawa/src/screens/Index.tsx
+++ ttarawa/src/screens/Index.tsx
@@ -9,13 +9,13 @@ export default function Index() {
       <Card
         imagepath={require('@assets/riding.png')}
         likes="15"
-        content="이번에 새로운 코스 달려봤는데 확실히 오랜만에 달리니까 너무 좋았습니다!! 이 코스 꼭 추천드립니다!"
+        content="이번에 새로운 코스 달려봤는데 확실히 오랜만에 달리니까 너무 좋았습니다!! 이 코스 꼭 추천드립니다!이번에 새로운 코스 달려봤는데새로운 코스 달려봤는데새로운 코스 달려봤새로운 코스 달려봤는데는데 확실히 오랜만에 달리니까 너무 좋았습니다!! 이 코스 꼭 추천드립니다!"
         isLike="true"
         distence="3.5km"
         hour="30분"
         isSns="true"
-        userName="열정라이더따옹이"
-        profileImage={require('@assets/logo.png')}
+        // userName="열정라이더따옹이"
+        userImg={require('@assets/profile.png')}
         rank={require('@assets/rank/racer.png')}
       />
     </View>
@@ -25,7 +25,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: color.primary,
+    backgroundColor: color.white,
     flexDirection: 'column',
     justifyContent: 'center',
   },
