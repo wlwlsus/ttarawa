e4c83145885e1dfa7888cbd853ebfe400888704c
고수연 <kosy1782@gmail.com>
2023-03-14 11:15:22 +0900

    BE:Refactor: edit start.sh file for chown

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 docker-compose -f docker-compose.yml pull
 
+chown -R $USER:$USER /var/jenkins_home/workspace/jenkins-back/ttrawa
+
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml up --build -d
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
