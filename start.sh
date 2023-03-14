3dfbec739f22609a9cec84430c3ee87c4efcedcf
고수연 <kosy1782@gmail.com>
2023-03-14 11:17:02 +0900

    BE:Refactor: remove chown cli

--- start.sh
+++ start.sh
@@ -2,6 +2,4 @@ docker-compose -f docker-compose.yml pull
 
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml up --build -d
 
-chown -R $USER:$USER /var/jenkins_home/workspace/jenkins-back/ttrawa
-
 docker rmi -f $(docker images -f "dangling=true" -q) || true
