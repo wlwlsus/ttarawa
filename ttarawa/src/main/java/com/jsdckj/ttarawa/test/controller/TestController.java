7407c59fffc51835a828fe7f69fa0899df5b6567
kosy318 <77595685+kosy318@users.noreply.github.com>
2023-03-15 10:27:55 +0900

    BE:Feat: add test api

---
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/test/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.jsdckj.ttarawa.test.controller;
+
+import com.jsdckj.ttarawa.util.Response;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/api/v1/test")
+@RequiredArgsConstructor
+@RestController
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<?> getLogs(@RequestBody String message){
+        return Response.ok(message);
+    }
+
+}
