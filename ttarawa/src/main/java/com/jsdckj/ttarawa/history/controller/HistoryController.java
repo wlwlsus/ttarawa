244065cd1cea54e8518ce04c4cfd199ecd2c1026
두소원 <sowondoo@naver.com>
2023-03-16 17:48:36 +0900

    BE: Feat: create history controller

--- ttarawa/src/main/java/com/jsdckj/ttarawa/history/controller/HistoryController.java
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/history/controller/HistoryController.java
@@ -1,4 +1,16 @@
 package com.jsdckj.ttarawa.history.controller;
 
+import com.jsdckj.ttarawa.history.service.HistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/history")
+@RequiredArgsConstructor
 public class HistoryController {
+
+  private final HistoryService historyService;
+
 }
