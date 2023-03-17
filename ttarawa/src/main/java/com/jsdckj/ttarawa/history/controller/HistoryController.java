3220faac27be6778089ecbc229fe97383d49522c
두소원 <sowondoo@naver.com>
2023-03-17 10:35:53 +0900

    BE: Feat: add history api in controller

--- ttarawa/src/main/java/com/jsdckj/ttarawa/history/controller/HistoryController.java
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/history/controller/HistoryController.java
@@ -1,10 +1,13 @@
 package com.jsdckj.ttarawa.history.controller;
 
+import com.jsdckj.ttarawa.history.dto.req.HistoryReqDto;
+import com.jsdckj.ttarawa.history.dto.req.HistoryUpdateReq;
 import com.jsdckj.ttarawa.history.service.HistoryService;
+import com.jsdckj.ttarawa.util.Response;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/history")
@@ -12,5 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class HistoryController {
 
   private final HistoryService historyService;
+
+  @PostMapping("/post/{user_id}")
+  public ResponseEntity<?> insertHistory(@PathVariable("user_id") Long userId, @RequestPart MultipartFile image, @RequestPart HistoryReqDto historyReqDto){
+    historyService.insertHistory(userId, image, historyReqDto);
+    return Response.ok("게시물 저장 성공");
+  }
+
+  @PutMapping("/post/{user_id}")
+  public ResponseEntity<?> updateHistory(@PathVariable("user_id") Long userId, @RequestParam("history_id") Long historyId, @RequestBody HistoryUpdateReq historyUpdateReq){
+    historyService.updateHistory(userId, historyId, historyUpdateReq);
+    return Response.ok("게시물 수정 성공");
+  }
+
+  @DeleteMapping("/post/{user_id}")
+  public ResponseEntity<?> deleteHistory(@PathVariable("user_id") Long userId, @RequestParam("history_id") Long historyId){
+    historyService.deleteHistory(userId,historyId);
+    return Response.ok("게시물 삭제 성공");
+  }
 
 }
