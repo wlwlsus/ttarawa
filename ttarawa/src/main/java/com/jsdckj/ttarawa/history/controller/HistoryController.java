1a20bb31b02106669d7ba732cdd5432cfa5e6e45
두소원 <sowondoo@naver.com>
2023-03-20 15:39:44 +0900

    BE: Comment: add api operation in HistoryController

--- ttarawa/src/main/java/com/jsdckj/ttarawa/history/controller/HistoryController.java
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/history/controller/HistoryController.java
@@ -6,6 +6,7 @@ import com.jsdckj.ttarawa.history.dto.res.HistoryResDto;
 import com.jsdckj.ttarawa.history.service.FavoriteService;
 import com.jsdckj.ttarawa.history.service.HistoryService;
 import com.jsdckj.ttarawa.util.Response;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -17,26 +18,25 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/v1/history")
+@RequestMapping("/api/v1/history/post")
 @RequiredArgsConstructor
-@Tag(name = "History", description = "주행기록 API, 좋아요 API")
-
+@Tag(name = "History", description = "주행기록 API")
 public class HistoryController {
 
   private final HistoryService historyService;
-  private final FavoriteService favoriteService;
-
   // post //
 
   // 게시물 저장
-  @PostMapping("/post/{user_id}")
+  @Operation(summary = "주행 기록 저장 API")
+  @PostMapping("/{user_id}")
   public ResponseEntity<?> insertHistory(@PathVariable("user_id") Long userId, @RequestPart MultipartFile image, @RequestPart HistoryReqDto historyReqDto) {
     historyService.insertHistory(userId, image, historyReqDto);
     return Response.ok("게시물 저장 성공");
   }
 
   // 게시물 1개 조회
-  @GetMapping("/post/detail/{history_id}")
+  @Operation(summary = "게시물 1개 조회 API")
+  @GetMapping("/detail/{history_id}")
   public ResponseEntity<?> selectOneHistory(@PathVariable("history_id") Long historyId) {
     Long userId = 38L; // 현재 유저 가져오기 -> jwt 복호화 하는 메소드 추가 해야함
     HistoryResDto historyResDto = historyService.selectOneHistory(userId, historyId);
@@ -48,14 +48,16 @@ public class HistoryController {
   }
 
   // 게시물 목록 조회
-  @GetMapping("/post")
+  @Operation(summary = "게시물 목록 조회 API")
+  @GetMapping
   public ResponseEntity<?> selectAllHistory(Pageable pageable) {
     Long userId = 38L; // 현재 유저 가져오기 -> jwt 복호화 하는 메소드 추가 해야함
     return Response.makeResponse(HttpStatus.OK, "게시물 조회에 성공했습니다", historyService.selectAllHistory(userId, pageable));
   }
 
   // 내 주행기록 조회
-  @GetMapping("/post/{user_id}")
+  @Operation(summary = "내 주행 기록 목록 조회 API")
+  @GetMapping("/{user_id}")
   public ResponseEntity<?> selectAllMyHistory(@PathVariable("user_id") Long userId, @PageableDefault(sort="createdDate",direction= Sort.Direction.DESC) Pageable pageable){
 
     return Response.makeResponse(HttpStatus.OK, "내 주행 기록 조회에 성공했습니다", historyService.selectAllMyHistory(userId, pageable));
@@ -64,6 +66,7 @@ public class HistoryController {
 
 
   // 게시물 수정
+  @Operation(summary = "게시물 수정 API")
   @PutMapping("/post/{user_id}")
   public ResponseEntity<?> updateHistory(@PathVariable("user_id") Long userId, @RequestParam("history_id") Long historyId, @RequestBody HistoryUpdateReq historyUpdateReq) {
     if (historyService.updateHistory(userId, historyId, historyUpdateReq))
@@ -74,42 +77,12 @@ public class HistoryController {
 
 
   // 게시물 삭제
+  @Operation(summary = "게시물 삭제 API")
   @DeleteMapping("/post/{user_id}")
   public ResponseEntity<?> deleteHistory(@PathVariable("user_id") Long userId, @RequestParam("history_id") Long historyId) {
     historyService.deleteHistory(userId, historyId);
     return Response.ok("게시물 삭제 성공");
   }
 
-  // favorite //
-
-
-  // 좋아요 등록
-  @PostMapping("/favorite/{user_id}")
-  public ResponseEntity<?> addFavorite(@PathVariable("user_id") Long userId, @RequestParam("history_id") Long historyId) {
-    boolean isSuccess = favoriteService.addFavorite(userId, historyId);
-    if (isSuccess) {
-      return Response.ok("좋아요 등록 성공");
-    } else {
-      return Response.badRequest("좋아요 등록 실패");
-    }
-  }
-
-  // 좋아요한 게시글 목록 조회
-  @GetMapping("/favorite/{user_id}")
-  public ResponseEntity<?> selectAllFavoriteHistory(@PathVariable("user_id") Long userId, Pageable pageable) {
-    return Response.makeResponse(HttpStatus.OK, "좋아요 한 게시글 목록 조회에 성공했습니다", favoriteService.selectAllFavoriteHistory(userId, pageable));
-  }
-
-
-  // 좋아요 삭제
-  @DeleteMapping("/favorite/{user_id}")
-  public ResponseEntity<?> deleteFavorite(@PathVariable("user_id") Long userId, @RequestParam("history_id") Long historyId) {
-    boolean isSuccess = favoriteService.deleteFavorite(userId, historyId);
-    if (isSuccess) {
-      return Response.ok("좋아요 제거 성공");
-    } else {
-      return Response.badRequest("좋아요 삭제 실패");
-    }
-  }
 
 }
