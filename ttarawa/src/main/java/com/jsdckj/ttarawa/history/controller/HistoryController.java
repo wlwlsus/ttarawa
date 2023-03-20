117b5d57d55ff98b9f95b5b0023f4dca23f5f0ba
두소원 <sowondoo@naver.com>
2023-03-20 15:00:40 +0900

    BE: Feat: add history get api in controller

--- ttarawa/src/main/java/com/jsdckj/ttarawa/history/controller/HistoryController.java
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/history/controller/HistoryController.java
@@ -2,11 +2,15 @@ package com.jsdckj.ttarawa.history.controller;
 
 import com.jsdckj.ttarawa.history.dto.req.HistoryReqDto;
 import com.jsdckj.ttarawa.history.dto.req.HistoryUpdateReq;
+import com.jsdckj.ttarawa.history.dto.res.HistoryResDto;
 import com.jsdckj.ttarawa.history.service.FavoriteService;
 import com.jsdckj.ttarawa.history.service.HistoryService;
 import com.jsdckj.ttarawa.util.Response;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,6 +34,36 @@ public class HistoryController {
     historyService.insertHistory(userId, image, historyReqDto);
     return Response.ok("게시물 저장 성공");
   }
+
+  // 게시물 1개 조회
+  @GetMapping("/post/{history_id}")
+  public ResponseEntity<?> selectOneHistory(@PathVariable("history_id") Long historyId) {
+    Long userId = 0L; // 현재 유저 가져오기 -> jwt 복호화 하는 메소드 추가 해야함
+    HistoryResDto historyResDto = historyService.selectOneHistory(userId, historyId);
+    if (historyResDto != null) {
+      return Response.makeResponse(HttpStatus.OK, "게시물 상세 조회 성공", historyResDto);
+    } else {
+      return Response.badRequest("게시물 없음");
+    }
+  }
+
+  // 게시물 목록 조회
+  @GetMapping("/post")
+  public ResponseEntity<?> selectAllHistory(Pageable pageable) {
+    Long userId = 0L; // 현재 유저 가져오기 -> jwt 복호화 하는 메소드 추가 해야함
+    return Response.makeResponse(HttpStatus.OK, "게시물 조회에 성공했습니다", historyService.selectAllHistory(userId, pageable));
+  }
+
+  // 내 주행기록 조회
+  @GetMapping("/post/{user_id}")
+  public ResponseEntity<?> selectAllMyHistory(@PathVariable("user_id") Long userId, @PageableDefault(sort="createdDate",direction= Sort.Direction.DESC) Pageable pageable){
+
+    return Response.makeResponse(HttpStatus.OK, "내 주행 기록 조회에 성공했습니다", historyService.selectAllMyHistory(userId, pageable));
+
+  }
+
+
+
 
   // 게시물 수정
   @PutMapping("/post/{user_id}")
@@ -63,6 +97,7 @@ public class HistoryController {
   }
 
   // 좋아요한 게시글 목록 조회
+  // TODO: pageable 객체로 받기
   @GetMapping("/favorite/{user_id}")
   public ResponseEntity<?> selectAllFavoriteHistory(@PathVariable("user_id") Long userId) {
     return Response.makeResponse(HttpStatus.OK, "좋아요 한 게시글 목록 조회에 성공했습니다", favoriteService.selectAllFavoriteHistory(userId));
