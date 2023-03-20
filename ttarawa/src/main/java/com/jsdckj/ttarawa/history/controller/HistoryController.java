84bd44176c0017ec4eeefd1e403095d17bee5bb5
두소원 <sowondoo@naver.com>
2023-03-20 17:26:37 +0900

    BE: Comment: add api operation

--- ttarawa/src/main/java/com/jsdckj/ttarawa/history/controller/HistoryController.java
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/history/controller/HistoryController.java
@@ -27,10 +27,10 @@ public class HistoryController {
   // post //
 
   // 게시물 저장
-  @Operation(summary = "주행 기록 저장 API", description = "user_id: 사용자 user_id\n" +
-      "image: Multipartfile\n" +
-      "personal: 1: 비공개, 0: 공개\n" +
-      "time: 초 단위\n" +
+  @Operation(summary = "주행 기록 저장 API", description = "user_id: 사용자 user_id\n\n" +
+      "image: Multipartfile => 아직 안됨 \n\n" +
+      "personal: 1: 비공개, 0: 공개\n\n" +
+      "time: 초 단위\n\n" +
       "distance: 미터 단위")
   @PostMapping("/{user_id}")
   public ResponseEntity<?> insertHistory(@PathVariable("user_id") Long userId, @RequestPart MultipartFile image, @RequestPart HistoryReqDto historyReqDto) {
@@ -39,12 +39,12 @@ public class HistoryController {
   }
 
   // 게시물 1개 조회
-  @Operation(summary = "게시물 1개 조회 API", description = "history_id: 주행 기록 게시물 번호\n" +
-      "favoritesCount: 좋아요 개수\n" +
-      "isMyFavorite: 1: 내가 좋아요를 누름, 0: 안누름\n" +
-      "time: 초단위\n" +
-      "distance: 미터단위\n" +
-      "image: 게시물 이미지\n")
+  @Operation(summary = "게시물 1개 조회 API", description = "history_id: 주행 기록 게시물 번호\n\n" +
+      "favoritesCount: 좋아요 개수\n\n" +
+      "isMyFavorite: 1: 내가 좋아요를 누름, 0: 안누름\n\n" +
+      "time: 초단위\n\n" +
+      "distance: 미터단위\n\n" +
+      "image: 게시물 이미지\n\n")
   @GetMapping("/detail/{history_id}")
   public ResponseEntity<?> selectOneHistory(@PathVariable("history_id") Long historyId) {
     Long userId = 38L; // 현재 유저 가져오기 -> jwt 복호화 하는 메소드 추가 해야함
@@ -57,16 +57,16 @@ public class HistoryController {
   }
 
   // 게시물 목록 조회
-  @Operation(summary = "게시물 목록 조회 API", description="최신순: ~~?page=0&sort=createdDate,desc\n" +
-      "좋아요순: ~~?page=0&sort=favoritesCount,desc\n" +
-      "추천순 미완\n" +
-      "swagger에서 요청시 \n" +
-      "{\n" +
-      "  \"page\": 0,\n" +
-      "  \"size\": 10,\n" +
-      "  \"sort\": [\n" +
+  @Operation(summary = "게시물 목록 조회 API", description="최신순: ~~?page=0&sort=createdDate,desc \\n" +
+      "좋아요순: ~~?page=0&sort=favoritesCount,desc \n\n" +
+      "추천순 미완 \n\n" +
+      "swagger에서 요청시 \n\n" +
+      "{\n\n" +
+      "  \"page\": 0(0부터 시작),\n\n" +
+      "  \"size\": 10,\n\n" +
+      "  \"sort\": [ \n\n" +
       "    \"createdDate,desc 또는 favoritesCount,desc 중 하나\"\n" +
-      "  ]\n" +
+      "  ]\n\n" +
       "}")
   @GetMapping
   public ResponseEntity<?> selectAllHistory(Pageable pageable) {
@@ -75,7 +75,7 @@ public class HistoryController {
   }
 
   // 내 주행기록 조회
-  @Operation(summary = "내 주행 기록 목록 조회 API")
+  @Operation(summary = "내 주행 기록 목록 조회 API", description = "~~?page=0 -> 0부터 시작")
   @GetMapping("/{user_id}")
   public ResponseEntity<?> selectAllMyHistory(@PathVariable("user_id") Long userId, @PageableDefault(sort="createdDate",direction= Sort.Direction.DESC) Pageable pageable){
 
@@ -85,7 +85,7 @@ public class HistoryController {
 
 
   // 게시물 수정
-  @Operation(summary = "게시물 수정 API")
+  @Operation(summary = "게시물 수정 API", description = "personal: 1: 비공개, 0: 공개")
   @PutMapping("/post/{user_id}")
   public ResponseEntity<?> updateHistory(@PathVariable("user_id") Long userId, @RequestParam("history_id") Long historyId, @RequestBody HistoryUpdateReq historyUpdateReq) {
     if (historyService.updateHistory(userId, historyId, historyUpdateReq))
