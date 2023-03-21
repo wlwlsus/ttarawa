f4f3744d2a1c91889a4d695e4a4272e8f1266a1d
kosy318 <77595685+kosy318@users.noreply.github.com>
2023-03-21 13:01:48 +0900

    BE:Refactor: edit codes for actuator

--- ttarawa/src/main/java/com/jsdckj/ttarawa/config/security/WebSecurityConfig.java
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/config/security/WebSecurityConfig.java
@@ -1,4 +1,34 @@
 package com.jsdckj.ttarawa.config.security;
 
+import com.jsdckj.ttarawa.jwt.JwtAuthenticationFilter;
+import com.jsdckj.ttarawa.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws
+            Exception {
+        httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("*").permitAll();
+        return httpSecurity.build();
+    }
 }
