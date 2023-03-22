fd03c66a82dbf8112adc43c91b7ff670645be562
두소원 <sowondoo@naver.com>
2023-03-22 16:20:14 +0900

    BE: Comment: add swagger operation

--- ttarawa/src/main/java/com/jsdckj/ttarawa/users/controller/UserController.java
+++ ttarawa/src/main/java/com/jsdckj/ttarawa/users/controller/UserController.java
@@ -7,6 +7,8 @@ import com.jsdckj.ttarawa.users.dto.req.UserReqDto;
 import com.jsdckj.ttarawa.users.dto.res.UserInfoResDto;
 import com.jsdckj.ttarawa.users.service.UserService;
 import com.jsdckj.ttarawa.util.Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,17 +25,19 @@ import static com.jsdckj.ttarawa.jwt.JwtProperties.TOKEN_HEADER;
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
+@Tag(name = "User", description = "User API")
 public class UserController {
 
-//  private final JwtUtil jwtUtil;
+  //  private final JwtUtil jwtUtil;
   private final UserService userService;
   private final JwtUtil jwtUtil;
 
 
+  @Operation(summary = "토큰 재발급 API")
   @PostMapping("/token")
-  public ResponseEntity<?> reissue(@RequestBody UserReqDto.Reissue reissue, Errors errors){
+  public ResponseEntity<?> reissue(@RequestBody UserReqDto.Reissue reissue, Errors errors) {
 
-    if(errors.hasErrors()){
+    if (errors.hasErrors()) {
       return Response.badRequest("토큰 재발급 실패");
     }
 
@@ -41,42 +45,45 @@ public class UserController {
 
   }
 
+  @Operation(summary = "로그아웃 API")
   @PostMapping("/logout")
-  public ResponseEntity<?> logout(@RequestBody UserReqDto.Logout logout, Errors errors){
-    if(errors.hasErrors()){
+  public ResponseEntity<?> logout(@RequestBody UserReqDto.Logout logout, Errors errors) {
+    if (errors.hasErrors()) {
       return Response.badRequest("로그아웃 실패");
     }
-    
+
     return userService.logout(logout);
   }
 
+  @Operation(summary = "회원 정보 API")
   @GetMapping("/{user_id}")
-  public ResponseEntity<?> getUserInfo(HttpServletRequest request, @PathVariable("user_id") Long userId){
-    Long headerUserId =jwtUtil.getUserId(request.getHeader(TOKEN_HEADER));
-    if(userId!=headerUserId){
+  public ResponseEntity<?> getUserInfo(HttpServletRequest request, @PathVariable("user_id") Long userId) {
+    Long headerUserId = jwtUtil.getUserId(request.getHeader(TOKEN_HEADER));
+    if (userId != headerUserId) {
       return Response.badRequest("사용자 불일치");
     }
 
     UserInfoResDto userInfoResDto = userService.getUserInfo(userId);
 
-    return Response.makeResponse(HttpStatus.OK, "회원 정보 조회 성공",userInfoResDto);
+    return Response.makeResponse(HttpStatus.OK, "회원 정보 조회 성공", userInfoResDto);
   }
 
 
-
+  @Operation(summary = "닉네임 변경 API")
   @PutMapping("/nickname/{user_id}")
-  public ResponseEntity<?> updateUserNickname(HttpServletRequest request, @PathVariable("user_id")Long userId, @RequestBody UserNicknameReqDto userNicknameReqDto){
-    Long headerUserId =jwtUtil.getUserId(request.getHeader(TOKEN_HEADER));
+  public ResponseEntity<?> updateUserNickname(HttpServletRequest request, @PathVariable("user_id") Long userId, @RequestBody UserNicknameReqDto userNicknameReqDto) {
+    Long headerUserId = jwtUtil.getUserId(request.getHeader(TOKEN_HEADER));
 
-    if(userId!=headerUserId){
+    if (userId != headerUserId) {
       return Response.badRequest("사용자 불일치");
     }
     userService.updateNickname(userId, userNicknameReqDto.getNickname());
     return Response.ok("닉네임 변경 성공");
   }
 
-  @PutMapping(value="/profile/{user_id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  public ResponseEntity<?> updateProfile(HttpServletRequest request, @PathVariable("user_id")Long userId, @RequestPart("image") MultipartFile multipartFile) throws IOException {
+  @Operation(summary = "프로필 사진 변경 API")
+  @PutMapping(value = "/profile/{user_id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<?> updateProfile(HttpServletRequest request, @PathVariable("user_id") Long userId, @RequestPart("image") MultipartFile multipartFile) throws IOException {
 //    Long headerUserId =jwtUtil.getUserId(request.getHeader(TOKEN_HEADER));
 //
 //    if(userId!=headerUserId){
@@ -86,8 +93,9 @@ public class UserController {
     return Response.ok("프로필 사진 변경 성공");
   }
 
+  @Operation(summary = "프로필 사진 샥제 API")
   @DeleteMapping("/profile/{user_id}")
-  public ResponseEntity<?> deleteProfile(HttpServletRequest request, @PathVariable("user_id")Long userId){
+  public ResponseEntity<?> deleteProfile(HttpServletRequest request, @PathVariable("user_id") Long userId) {
 //    Long headerUserId =jwtUtil.getUserId(request.getHeader(TOKEN_HEADER));
 //
 //    if(userId!=headerUserId){
@@ -97,7 +105,6 @@ public class UserController {
     userService.deleteProfile(userId);
     return Response.ok("프로필 사진 삭제 성공");
   }
-
 
 
 }
