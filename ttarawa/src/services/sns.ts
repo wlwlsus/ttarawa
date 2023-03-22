6b612b385de8969d547c62df627a8f3c5cc0ff93
Yeji <yzii9841@gmail.com>
2023-03-22 16:51:09 +0900

    FE:Feat: API request

--- ttarawa/src/services/sns.ts
+++ ttarawa/src/services/sns.ts
@@ -1,6 +1,15 @@
 import apiRequest from '@utils/apiRequest'
 
 // GET Request
+const fetchPost = async (sort: string, page: number): Promise<object> => {
+  return await apiRequest
+    .get(`history/post/all?sort=${sort}&page=${page}`)
+    .then((res) => {
+      return Promise.resolve(res.data.result)
+    })
+    .catch((err) => Promise.reject(err.data))
+}
+
 const fetchDetail = async (historyId: number): Promise<object> => {
   return await apiRequest
     .get(`history/post/detail/${historyId}`)
@@ -10,5 +19,15 @@ const fetchDetail = async (historyId: number): Promise<object> => {
     .catch((err) => Promise.reject(err.data))
 }
 
-const sns = { fetchDetail }
+// DELETE Request
+const deletePost = async (historyId: number): Promise<object> => {
+  return await apiRequest
+    .delete(`history/posty?history_id=${historyId}`)
+    .then((res) => {
+      return Promise.resolve(res.data.message)
+    })
+    .catch((err) => Promise.reject(err.data))
+}
+
+const sns = { fetchPost, fetchDetail, deletePost }
 export default sns
