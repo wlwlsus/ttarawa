8223bd639173247aeec0efe39479580e26afe08e
kosy318 <77595685+kosy318@users.noreply.github.com>
2023-03-23 10:01:26 +0900

    BE:Feat: nginx loadbalancing

--- ttarawa/deploy.sh
+++ ttarawa/deploy.sh
@@ -7,6 +7,7 @@ function create_docker_image_blue(){
   ./gradlew clean build
 
   docker build -t app:0.1 .
+  docker build -t app:0.15 .
 
 }
 
@@ -17,19 +18,23 @@ function create_docker_image_green(){
   ./gradlew clean build
 
   docker build -t app:0.2 .
+  docker build -t app:0.25 .
 }
 
 function execute_blue(){
     docker ps -q --filter "name=app_blue" || grep -q . && docker stop app_blue && docker rm app_blue || true
+    docker ps -q --filter "name=app_skyblue" || grep -q . && docker stop app_skyblue && docker rm app_skyblue || true
 
     sleep 10
 
     docker-compose -p app-blue -f docker-compose.blue.yml up -d
+    docker-compose -p app-skyblue -f docker-compose.skyblue.yml up -d
 
     sleep 10
 
     echo "GREEN:8086 종료"
     docker-compose -p app-green -f docker-compose.green.yml down
+    docker-compose -p app-yellowgreen -f docker-compose.yellowgreen.yml down
 
     #dangling=true : 불필요한 이미지 지우기
     docker rmi -f $(docker images -f "dangling=true" -q) || true
@@ -37,14 +42,17 @@ function execute_blue(){
 
 function execute_green(){
   docker ps -q --filter "name=app_green" || grep -q . && docker stop app_green && docker rm app_green || true
+  docker ps -q --filter "name=app_yellowgreen" || grep -q . && docker stop app_yellowgreen && docker rm app_yellowgreen || true
 
     echo "GREEN:8086 실행"
     docker-compose -p app-green -f docker-compose.green.yml up -d
+    docker-compose -p app-yellowgreen -f docker-compose.yellowgreen.yml up -d
 
     sleep 10
 
     echo "BLUE:8085 종료"
     docker-compose -p app-blue -f docker-compose.blue.yml down
+    docker-compose -p app-skyblue -f docker-compose.skyblue.yml down
 
     #dangling=true : 불필요한 이미지 지우기
     docker rmi -f $(docker images -f "dangling=true" -q) || true
@@ -73,6 +81,7 @@ if [ -z ${RUNNING_GREEN} ]
       sleep 10
 
       docker-compose -p app-blue -f docker-compose.blue.yml up -d
+      docker-compose -p app-skyblue -f docker-compose.skyblue.yml up -d
 	  
     else
       # 8086포트로 어플리케이션 구동
