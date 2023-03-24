f21c012c46369a711d34563f219ace4e504aae3e
성원준 <cadqe13@gmail.com>
2023-03-24 17:51:51 +0900

    BE:Feat: Implement user average distance-based recommendation lists

--- ttarawa-cf/app/services/recommendation.py
+++ ttarawa-cf/app/services/recommendation.py
@@ -22,13 +22,35 @@ def haversine(lat1, lng1, lat2, lng2):
     return distance
 
 
-def get_user_similar_destinations(user_info, data, num_destinations=10):
-    df = pd.read_csv(data, encoding='cp949')
-    vectors = df[['rating', 'reviews', 'search']].values
-    similarities = cosine_similarity([user_info], vectors)
-    similar_destinations_indices = similarities[0].argsort()[::-1][:num_destinations]
-    similar_destinations = df.loc[similar_destinations_indices]
-    return similar_destinations
+def get_user_similar_destinations(user_info, lat, lng, num_destinations=10):
+    try:
+        tours = Tour.query.with_entities(
+            Tour.tour_id, Tour.address, Tour.category, Tour.lat, Tour.lng,
+            Tour.mid_category, Tour.name, Tour.rating, Tour.reviews,
+            Tour.search, Tour.sub_category
+        ).all()
+
+        df = pd.DataFrame(tours,
+                          columns=['tour_id', 'address', 'category', 'lat', 'lng', 'mid_category', 'name', 'rating',
+                                   'reviews', 'search', 'sub_category'])
+        # 거리 계산
+        distances = df.apply(lambda row: haversine(lat, lng, row['lat'], row['lng']), axis=1)
+        df['distances'] = distances
+        similar_destinations_indices = distances[distances.between(user_info - 0.5, user_info + 0.5)].index.tolist()
+
+        # # 유사도 계산
+        # vectors = df.loc[similar_destinations_indices, ['rating', 'reviews', 'search']].values
+        # similarities = cosine_similarity([user_info] * len(similar_destinations_indices), vectors)
+        # similar_destinations_indices = [x for _, x in
+        #                                 sorted(zip(similarities[0], similar_destinations_indices), reverse=True)]
+
+        # 추천 목적지 반환
+        similar_destinations = df.loc[similar_destinations_indices][:num_destinations]
+        similar_destinations = similar_destinations.sort_values(by=['rating', 'reviews'], ascending=[False, False])
+        return similar_destinations
+    except Exception as e:
+        print(f'Flask Server Error : {e}')
+        abort(500, str(e))
 
 
 def get_nearby_destinations(lat, lng, min_distance=0, max_distance=1, num_destinations=10):
@@ -59,8 +81,8 @@ def get_nearby_destinations(lat, lng, min_distance=0, max_distance=1, num_destin
 
 
 def get_recommendations(lat, lng, min_distance=0, max_distance=1, num_destinations=10, user_info=None):
-    if user_info is not None:
-        similar_destinations = get_user_similar_destinations(user_info, data, num_destinations)
+    if user_info > 0:
+        similar_destinations = get_user_similar_destinations(user_info, lat, lng, num_destinations)
         return similar_destinations
     else:
         nearby_destinations = get_nearby_destinations(lat, lng, min_distance, max_distance, num_destinations)
