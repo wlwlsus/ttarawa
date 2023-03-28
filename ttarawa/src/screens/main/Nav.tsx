d2af5c00bc32f71043416f3e242a677f9d60be6e
CSB <tph5268@naver.com>
2023-03-28 11:37:49 +0900

    FE:Feat: locationData recoil 적용

--- ttarawa/src/screens/main/Nav.tsx
+++ ttarawa/src/screens/main/Nav.tsx
@@ -4,18 +4,21 @@ import { styles } from '@styles/GlobalStyles'
 import { map } from '@styles/main'
 import { SafeAreaView } from 'react-native'
 import * as Location from 'expo-location'
-
-type LocationObject = Location.LocationObject
+import { useRecoilState } from 'recoil'
+import { locationListState } from '~/store/atoms'
 
 export default function Map() {
+  type LocationObject = Location.LocationObject
+
   const [isEnabled, setIsEnabled] = useState(false)
-  const [locationData, setLocationData] = useState<
-    { latitude: number; longitude: number }[]
-  >([])
+  const [locationData, setLocationData] = useRecoilState(locationListState)
+
   const [watchId, setWatchId] = useState<number | null>(null)
 
+  // 토글로 위치정보 저장 on/off -> 버튼으로 바꿀 예정
   const toggleSwitch = async () => {
     setIsEnabled((previousState) => !previousState)
+    // 위치정보 저장 on 이라면...
     if (!isEnabled) {
       await Location.requestForegroundPermissionsAsync()
       const newWatchId = await Location.watchPositionAsync(
@@ -26,10 +29,13 @@ export default function Map() {
         },
         (location) => {
           const { latitude, longitude } = location.coords
-          setLocationData((prevData) => [...prevData, { latitude, longitude }])
+          const newData = [...locationData, { latitude, longitude }]
+          setLocationData(newData)
         },
       )
       setWatchId(newWatchId)
+
+      // off라면...
     } else {
       if (watchId) {
         Location.clearWatch(watchId)
---
+++ ttarawa/src/store/atoms.ts
@@ -0,0 +1,51 @@
+import { atom } from 'recoil'
+
+export const userState = atom({
+  key: 'userState', // 고유한 값
+  default: {
+    img: '',
+    userName: '따옹이',
+    rank: 'junior',
+    riding: '32',
+  },
+})
+
+export const departState = atom<object>({
+  key: 'departState',
+  default: {
+    title: undefined,
+    lat: undefined,
+    lng: undefined,
+  },
+})
+
+export const destinState = atom({
+  key: 'destinState',
+  default: {
+    title: undefined,
+    lat: undefined,
+    lng: undefined,
+  },
+})
+
+export const markerListState = atom({
+  key: 'markerListState',
+  default: [],
+})
+
+export const markerState = atom({
+  key: 'markerState',
+  default: {
+    address: undefined,
+    distance: undefined,
+    lat: undefined,
+    lng: undefined,
+    name: undefined,
+    spotId: undefined,
+  },
+})
+
+export const locationListState = atom({
+  key: 'locationListState',
+  default: [],
+})
